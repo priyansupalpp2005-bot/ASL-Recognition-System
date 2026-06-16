483d58f9766c89ab1fa56366a93c3c422e3284b6
Priyansu pal <priyansu.pal.pp2005@gmail.com>
2026-06-16 17:35:54 +0530

    Added WebRTC camera

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import av
-#import cv2
-#import mediapipe as mp
+import cv2
+import mediapipe as mp
 import numpy as np
 import joblib
 #import pyttsx3
@@ -203,19 +203,40 @@ st.markdown("""
 # CAMERA
 # ======================
 class VideoProcessor(VideoProcessorBase):
+
+    def __init__(self):
+        self.hands = mp.solutions.hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+
     def recv(self, frame):
+
         img = frame.to_ndarray(format="bgr24")
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-if run:
-    st.success("WebRTC section reached")
+        rgb = cv2.cvtColor(
+            img,
+            cv2.COLOR_BGR2RGB
+        )
 
-    webrtc_streamer(
-        key="camera",
-        video_processor_factory=VideoProcessor,
-        media_stream_constraints={
-            "video": True,
-            "audio": False,
-        },
-        async_processing=True,
-    )
+        results = self.hands.process(rgb)
+
+        if results.multi_hand_landmarks:
+
+            for hand_landmarks in results.multi_hand_landmarks:
+
+                self.mp_draw.draw_landmarks(
+                    img,
+                    hand_landmarks,
+                    self.mp_hands.HAND_CONNECTIONS
+                )
+
+        return av.VideoFrame.from_ndarray(
+            img,
+            format="bgr24"
+        )
